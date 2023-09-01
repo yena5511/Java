9363778e5a318001e49d0c98d0a5069562990db1
yena5511 <ar01077422264@gmail.com>
2023-09-01 22:45:07 +0900

    0901

---
+++ src/DesignPattern/Command/Command.java
@@ -0,0 +1,36 @@
+package DesignPattern.Command;
+
+public class Command {
+    protected Robot robot;
+
+    public void setRobot (Robot _robot){
+        this.robot = _robot;
+    }
+    public abstract void execute();
+}
+
+class MoveForwardCommand extends Command{
+    int space;
+    public MoveForwardCommand (int _space){
+        space = _space;
+    }
+    public void execute(){
+        robot.moveForward(space);
+    }
+}
+
+class TurnCommand extends Command{
+    Robot.Direction direction;
+    public TurnCommand (Robot.Direction _direction){
+        direction = _direction;
+    }
+    public void execute (){
+        robot.turn(direction);
+    }
+}
+
+class PickupCommand extends Command{
+    public void excute (){
+        robot.pickup();
+    }
+}
---
+++ src/DesignPattern/Command/RobotKit.java
@@ -0,0 +1,17 @@
+package DesignPattern.Command;
+
+import java.util.ArrayList;
+public class RobotKit {
+    private Robot robot = new Robot();
+    private ArrayList<Command> commands = new ArrayList<Command>();
+
+    public void addCommand (Command command){
+        commands.add(command);
+    }
+    public void start (){
+        for (Command command : commands){
+            command.setRobot(robot);
+            command.execute();
+        }
+    }
+}
